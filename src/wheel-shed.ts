c57a0ba0379dbf50b65a273db07fba81170cecde
BrinkerVII <brinkervii@gmail.com>
2017-10-23 22:01:58 +0200

    Init metadata and add wheel

--- src/wheel-shed.ts
+++ src/wheel-shed.ts
@@ -4,15 +4,20 @@ import * as path from 'path';
 
 const d = debug('wheel-shed');
 
+const OBJECTS_FOLDER = "objects";
+const METADATA_FILENAME = ".wheel-metadata.json";
+
 export class WheelShed {
 	private objectsDirectory: string;
+	private metaDataPath: string;
 
 	constructor(private basePath: string) {
 		this.initPath(this.basePath)
 			.then(() => {
 				d(`Base path initialized: ${this.basePath}`);
-				this.objectsDirectory = path.join(this.basePath, "objects");
-				
+				this.objectsDirectory = path.join(this.basePath, OBJECTS_FOLDER);
+				this.metaDataPath = path.join(this.basePath, METADATA_FILENAME);
+
 				this.initPath(this.objectsDirectory)
 					.then(() => {
 						d(`Objects path initialized: ${this.objectsDirectory}`);
@@ -20,12 +25,21 @@ export class WheelShed {
 					.catch(err => {
 						d(`Failed to initialize objects directory: ${this.objectsDirectory}`);
 						console.error(err);
+					});
+					
+				this.touchFile(this.metaDataPath)
+					.then(() => {
+						d(`Initialized metadata file: ${this.metaDataPath}`);
 					})
+					.catch(err => {
+						d(`Failed to initialize metadata file: ${this.metaDataPath}`);
+						console.error(err);
+					});
 			})
 			.catch(err => {
 				d(`Failed to initialize base path: ${this.basePath}`);
 				console.error(err);
-			})
+			});
 	}
 
 	private initPath(pathString: string): Promise<void> {
@@ -50,6 +64,35 @@ export class WheelShed {
 					mkdirs();
 					console.error(err);
 				})
+		});
+	}
+
+	private touchFile(filePath: string): Promise<void> {
+		return new Promise<void>((resolve, reject) => {
+			let createFile = (): Promise<void> => {
+				return new Promise<void>((resolve, reject) => {
+					fsextra.createFileAsync(filePath)
+						.then(resolve)
+						.catch(reject);
+				});
+			}
+
+			fsextra.existsAsync(filePath)
+				.then(exists => {
+					if (exists) {
+						resolve();
+					} else {
+						createFile()
+							.then(resolve)
+							.catch(reject);
+					}
+				})
+				.catch(err => {
+					console.error(err);
+					createFile()
+						.then(resolve)
+						.catch(reject);
+				});
 		});
 	}
 }

d9389545a9496a4c0623f214fe71ed300759ade8
BrinkerVII <brinkervii@gmail.com>
2017-10-24 00:45:15 +0200

    Mod ftest

--- src/ftest/main.ts
+++ src/ftest/main.ts
@@ -3,11 +3,11 @@ import { Wheel } from "../wheel";
 
 const shed = new WheelShed("./tmp")
 shed.ready().then(() => {
-	let wheel = new Wheel(shed);
-	shed.addWheel(wheel)
-		.then(() => {
-			console.log("Added wheel to shed");
-		})
-		.catch(console.error);
-
+	// let wheel = new Wheel(shed);
+	// shed.addWheel(wheel)
+	// 	.then(() => {
+	// 		console.log("Added wheel to shed");
+	// 	})
+	// 	.catch(console.error);
+	console.log(shed);
 });
